081f7716690d36897298a3cb72161a3664ad36ab
chiennc4805 <chiennguyen5981@gmail.com>
2025-06-03 13:08:48 +0700

    update UpdateProfileServlet, update ImageUploader
    update max size of uploaded file to 5MB; update user in session

--- src/main/java/util/ImageUploader.java
+++ src/main/java/util/ImageUploader.java
@@ -13,7 +13,7 @@ public class ImageUploader {
         File file = new File(uploadsDir, fileName);
         try (InputStream inputData = uploadedFile.getInputStream();
              OutputStream out = new FileOutputStream(file)) {
-            byte[] buffer = new byte[1024];
+            byte[] buffer = new byte[5120];
             int length;
             while ((length = inputData.read(buffer)) != -1) {
                 out.write(buffer, 0, length);
