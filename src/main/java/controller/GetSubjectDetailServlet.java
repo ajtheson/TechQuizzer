a5900e3accac10ca8a173b5811c98554cb8e58b7
Ud248 <141844455+Ud248@users.noreply.github.com>
2025-06-11 22:38:56 +0700

    Add comment in GetSubjectDetailServlet and GetSubjectListServlet

--- src/main/java/controller/GetSubjectDetailServlet.java
+++ src/main/java/controller/GetSubjectDetailServlet.java
@@ -18,17 +18,24 @@ public class GetSubjectDetailServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        //Init DAO object needed
         SubjectDAO subjectDAO = new SubjectDAO();
         PricePackageDAO pricePackageDAO = new PricePackageDAO();
-        String subjectParam = request.getParameter("subject_id");
-        System.out.println(subjectParam);
 
-        int subjectId = Integer.parseInt(subjectParam);
+        //Get subjectId from request
+        int subjectId = Integer.parseInt(request.getParameter("subject_id"));
 
+        //Get subject to show detail
         Subject subject = subjectDAO.getSubjectById(subjectId);
+
+        //Get active price packages of this subject to show
         List<PricePackage> pricePackages = pricePackageDAO.getActiveOfSubject(subjectId);
+
+        //Get min list price and sale price to show
         double minListPrice = pricePackageDAO.getMinListPrice(subjectId);
         double minSalePrice = pricePackageDAO.getMinSalePrice(subjectId);
+
+        //Count discount
         int discount = (int)Math.ceil((minListPrice * 1.0 - minSalePrice)/minListPrice * 100);
 
         request.setAttribute("subject", subject);
@@ -36,7 +43,6 @@ public class GetSubjectDetailServlet extends HttpServlet {
         request.setAttribute("minListPrice", minListPrice);
         request.setAttribute("minSalePrice", minSalePrice);
         request.setAttribute("discount", discount);
-
         request.getRequestDispatcher("subject_detail.jsp").forward(request, response);
     }
 }
--- src/main/java/controller/GetSubjectListServlet.java
+++ src/main/java/controller/GetSubjectListServlet.java
@@ -24,78 +24,73 @@ public class GetSubjectListServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        //Init default for page, size, search, isFeatured, categoryId, sortOrder
         int page = 1;
         int size = 5;
         String search = "";
         boolean isFeatured = false;
         int categoryId = 0;
         boolean sortOrder = true;
-        int userID = 0;
 
+        //Get userID from session
+        int userID = 0;
         HttpSession session = request.getSession();
-        if(session.getAttribute("user") != null) {
-            UserDTO user = (UserDTO)session.getAttribute("user");
+        if (session.getAttribute("user") != null) {
+            UserDTO user = (UserDTO) session.getAttribute("user");
             userID = user.getId();
         }
 
+        //Get page, size, search, isFeatured, categoryId, sortOrder from request
         if (request.getParameter("page") != null) {
             page = Integer.parseInt(request.getParameter("page"));
         }
-
         if (request.getParameter("size") != null) {
             size = Integer.parseInt(request.getParameter("size"));
         }
-
         if (request.getParameter("search") != null) {
             search = request.getParameter("search");
         }
-
         if (request.getParameter("isFeatured") != null) {
             isFeatured = Boolean.parseBoolean(request.getParameter("isFeatured"));
         }
-
         if (request.getParameter("categoryId") != null) {
             categoryId = Integer.parseInt(request.getParameter("categoryId"));
         }
-
         if (request.getParameter("sortOrder") != null) {
             sortOrder = request.getParameter("sortOrder").equalsIgnoreCase("desc");
         }
 
-
-        SubjectDAO subjectDAO = new SubjectDAO ();
+        //Init DAO object needed
+        SubjectDAO subjectDAO = new SubjectDAO();
         CategoryDAO categoryDAO = new CategoryDAO();
         RegistrationDAO registrationDAO = new RegistrationDAO();
 
-        //Get all subjects, categories from database and set to request attribute
+        //Get all subjects from database and convert them to subjectDTO
         List<Subject> subjectList = subjectDAO.getAllSubjectsWithPagination(page, size, categoryId, sortOrder, isFeatured, search);
         List<SubjectDTO> subjects = new ArrayList<>();
         SubjectService subjectService = new SubjectService();
-        for(Subject subject : subjectList) {
+        for (Subject subject : subjectList) {
             SubjectDTO subjectDTO = subjectService.toSubjectDTO(subject);
+            //If the user has already registered for that course, they are not allowed to register again.
             subjectDTO.setRegistered(registrationDAO.isRegistrationExist(userID, subjectDTO.getId()));
             subjects.add(subjectDTO);
         }
+
+        //Get total subjects, total pages and categories to show in subject list page
         int totalSubjects = subjectDAO.getTotalSubjects(categoryId, isFeatured, search);
         int totalPages = (int) Math.ceil((double) totalSubjects / size);
-
         List<Category> categories = categoryDAO.getAllCategory();
-
 
         request.setAttribute("subjects", subjects);
         request.setAttribute("categories", categories);
-
         request.setAttribute("totalSubjects", totalSubjects);
-
         request.setAttribute("page", page);
         request.setAttribute("totalPages", totalPages);
         request.setAttribute("size", size);
-
         request.setAttribute("search", search);
         request.setAttribute("isFeatured", isFeatured ? "true" : "");
         request.setAttribute("categoryId", categoryId);
         request.setAttribute("sortOrder", sortOrder ? "desc" : "asc");
-
         request.getRequestDispatcher("subject_list.jsp").forward(request, response);
     }
 }
